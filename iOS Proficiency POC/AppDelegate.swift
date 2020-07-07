b2fcf5093a06e24e1e993369893a73712f0d0e95
Prasanna <Prasanapodapati@gmail.com>
2020-07-07 10:43:37 +0530

    Commit to support iOS lower versions up to iOS 11.0

--- iOS Proficiency POC/AppDelegate.swift
+++ iOS Proficiency POC/AppDelegate.swift
@@ -8,23 +8,60 @@
 
 import UIKit
 
-@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        if #available(iOS 13.0, *) {
+            // In iOS 13 setup is done in SceneDelegate
+        } else {
+            let viewController = ProductTableViewController()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            window?.rootViewController = navigationController
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window!.rootViewController = navigationController
+        }
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            // iOS 13 setup is done in SceneDelegate
+        } else {
+            self.window?.makeKeyAndVisible()
+        }
 
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+         // iOS 13 setup is done in SceneDelegate
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+         // iOS 13 setup is done in SceneDelegate
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // iOS 13 setup is done in SceneDelegate
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+         // iOS 13 setup is done in SceneDelegate
+    }
+
+
+    // MARK: UISceneSession Lifecycle
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
